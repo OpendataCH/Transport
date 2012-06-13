b9c0ba95b534cca6e48488209146750aa630986c
Fabian Vogler <fabian@equivalence.ch>
2012-06-13 07:59:07 +0200

    Use Redis config

--- web/stats.php
+++ web/stats.php
@@ -9,8 +9,13 @@ date_default_timezone_set('Europe/Zurich');
 
 // init
 $app = new Silex\Application();
-$app['debug'] = true;
 
+// load config
+require __DIR__.'/../config/default.php';
+$local = __DIR__.'/../config/local.php';
+if (stream_resolve_include_path($local)) {
+	$local = include $local;
+}
 
 // autoload
 $app['autoloader']->registerNamespace('Predis', __DIR__.'/../vendor/predis/lib');
@@ -22,33 +27,35 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.class_path' => __DIR__.'/../vendor/twig/lib',
 ));
 
-$app['redis'] = new Predis\Client(array('host' => 'tetra.redistogo.com', 'port' => 9464, 'password' => '7cd7bdf5a51d601547da3c96d6bae1a2'));
+if ($app['redis.config']) {
+	$app['redis'] = new Predis\Client($app['redis.config']);
 
-// home
-$app->get('/', function(Request $request) use ($app) {
+	// home
+	$app->get('/', function(Request $request) use ($app) {
 
-    // read last 30 days
-    $dates = array();
-    $keys = array();
-    for ($i = 30; $i >= 0; $i--) {
-        $date = date('Y-m-d', strtotime("-$i days"));
-        $keys[] = "stats:calls:$date";
-        $dates[] = $date;
-    }
-    $values = array_combine($dates, $app['redis']->mget($keys));
+	    // read last 30 days
+	    $dates = array();
+	    $keys = array();
+	    for ($i = 30; $i >= 0; $i--) {
+	        $date = date('Y-m-d', strtotime("-$i days"));
+	        $keys[] = "stats:calls:$date";
+	        $dates[] = $date;
+	    }
+	    $values = array_combine($dates, $app['redis']->mget($keys));
 
-    // transform to comma and new line separated list
-    $data = array();
-    foreach ($values as $date => $value) {
-        $data[] = $date . ',' . ($value ?: 0);
-    }
-    $data = implode('\n', $data);
+	    // transform to comma and new line separated list
+	    $data = array();
+	    foreach ($values as $date => $value) {
+	        $data[] = $date . ',' . ($value ?: 0);
+	    }
+	    $data = implode('\n', $data);
 
-    return $app['twig']->render('stats.twig', array(
-        'values' => $values,
-        'data' => $data,
-    ));
-});
+	    return $app['twig']->render('stats.twig', array(
+	        'values' => $values,
+	        'data' => $data,
+	    ));
+	});
+}
 
 
 // run
