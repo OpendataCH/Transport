9ae64022f2200962a6cbe7dfb6cd5b08e17af14d
Fabian Vogler <fabian@equivalence.ch>
2012-06-13 13:54:17 +0200

    Disable debug on Pagoda Box

--- config/pagodabox.php
+++ config/pagodabox.php
@@ -1,6 +1,6 @@
 <?php
 
-$app['debug'] = true;
+$app['debug'] = false;
 $app['http_cache'] = true;
 
 $app['redis.config'] = array('host' => $_SERVER['CACHE1_HOST'], 'port' => $_SERVER['CACHE1_PORT']);
