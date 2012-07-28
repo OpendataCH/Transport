5e80ba408d251b9eaad35623866dca8b39034964
Fabian Vogler <fabian@equivalence.ch>
2012-07-28 23:14:54 +0200

    Added capacity to Journey

--- lib/Transport/Entity/Schedule/Journey.php
+++ lib/Transport/Entity/Schedule/Journey.php
@@ -36,7 +36,17 @@ class Journey
      * @var array
      */
     public $passList = array();
-    
+
+    /**
+     * @var int
+     */
+    public $capacity1st = null;
+
+    /**
+     * @var int
+     */
+    public $capacity2nd = null;
+
     static public function createFromXml(\SimpleXMLElement $xml, \DateTime $date, Journey $obj = null, $parentField = '')
     {
         if (!$obj) {
@@ -70,15 +80,34 @@ class Journey
             }
         }
 
+        $capacities1st = array();
+        $capacities2nd = array();
+
         $field = $parentField.'/passList';
         if (ResultLimit::isFieldSet($field)) {
             if ($xml->PassList->BasicStop) {
                 foreach ($xml->PassList->BasicStop AS $basicStop) {
                     if ($basicStop->Arr || $basicStop->Dep) {
-                        $obj->passList[] = Stop::createFromXml($basicStop, $date, null, $field);
+                        $stop = Stop::createFromXml($basicStop, $date, null, $field);
+                        if ($stop->prognosis->capacity1st) {
+                            $capacities1st[] = (int) $stop->prognosis->capacity1st;
+                        }
+                        if ($stop->prognosis->capacity2nd) {
+                            $capacities2nd[] = (int) $stop->prognosis->capacity2nd;
+                        }
+                        $obj->passList[] = $stop;
                     }
                 }
             }
+        }
+
+        $field = $parentField.'/capacity1st';
+        if (ResultLimit::isFieldSet($field) && count($capacities1st) > 0) {
+            $obj->capacity1st = max($capacities1st);   
+        }
+        $field = $parentField.'/capacity2nd';
+        if (ResultLimit::isFieldSet($field) && count($capacities2nd) > 0) {
+            $obj->capacity2nd = max($capacities2nd);   
         }
 
         return $obj;
