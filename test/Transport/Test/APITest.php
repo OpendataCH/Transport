c0d741fe62c3733c97fd0650d972b31c5745466f
Fabian Vogler <fabian@equivalence.ch>
2013-10-15 22:34:13 +0200

    More advanced delay test to reproduce #75

--- test/Transport/Test/APITest.php
+++ test/Transport/Test/APITest.php
@@ -144,16 +144,16 @@ class APITest extends \PHPUnit_Framework_TestCase
                         'Content-Type: application/xml'
                 )),
                 $this->equalTo('<?xml version="1.0" encoding="iso-8859-1"?>
-<ReqC lang="EN" prod="iPhone3.1" ver="2.3" accessId="YJpyuPISerpXNNRTo50fNMP0yVu7L6IMuOaBgS0Xz89l3f6I3WhAjnto4kS9oz1"><STBReq boardType="DEP" maxJourneys="40"><Time>22:05</Time><Period><DateBegin><Date>20131015</Date></DateBegin><DateEnd><Date>20131015</Date></DateEnd></Period><TableStation externalId="008591052"/><ProductFilter>1111111111111111</ProductFilter></STBReq></ReqC>
+<ReqC lang="EN" prod="iPhone3.1" ver="2.3" accessId="YJpyuPISerpXNNRTo50fNMP0yVu7L6IMuOaBgS0Xz89l3f6I3WhAjnto4kS9oz1"><STBReq boardType="DEP" maxJourneys="40"><Time>22:20</Time><Period><DateBegin><Date>20131015</Date></DateBegin><DateEnd><Date>20131015</Date></DateEnd></Period><TableStation externalId="008591052"/><ProductFilter>1111111111111111</ProductFilter></STBReq></ReqC>
 ')
             )
             ->will($this->returnValue($response));
 
         $station = new Station('008591052'); // Zürich, Bäckeranlage
-        $journeys = $this->api->getStationBoard(new StationBoardQuery($station, \DateTime::createFromFormat(\DateTime::ISO8601, '2013-10-15T22:05:00+01:00')));
+        $journeys = $this->api->getStationBoard(new StationBoardQuery($station, \DateTime::createFromFormat(\DateTime::ISO8601, '2013-10-15T22:20:00+01:00')));
 
         $this->assertEquals(1, count($journeys));
         $this->assertEquals('2013-10-15T22:10:00+0100', $journeys[0]->stop->departure);
-        $this->assertEquals(2, $journeys[0]->stop->delay);
+        $this->assertEquals(12, $journeys[0]->stop->delay);
     }
 }
