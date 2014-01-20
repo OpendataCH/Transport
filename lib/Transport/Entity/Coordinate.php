OpendataCH/Transport

commit 8e981ec8e9586d5779143627ade0f155a22c8069

Fixed for HAFAS inverted coordinates
Fixes #81

lib/Transport/Entity/Coordinate.php
@@ -39,8 +39,17 @@ class Coordinate
     {
         $coordinate = new Coordinate();
         $coordinate->type = 'WGS84'; // best guess
-        $coordinate->x = self::intToFloat($json->x);
-        $coordinate->y = self::intToFloat($json->y);
+
+        $x = self::intToFloat($json->x);
+        $y = self::intToFloat($json->y);
+
+        if ($y > $x) { // HAFAS bug, returns inverted lat/long
+          $coordinate->x = $y;
+          $coordinate->y = $x;
+        } else {
+          $coordinate->x = $x;
+          $coordinate->y = $y;
+        }
 
         return $coordinate;
     }

test/Transport/Test/APITest.php
@@ -69,12 +69,12 @@ class APITest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(2, count($stations));
         $this->assertEquals(8508489, $stations[0]->id);
         $this->assertEquals('Kehrsiten-Bürgenstock', $stations[0]->name);
-        $this->assertEquals(8.382324, $stations[0]->coordinate->x);
-        $this->assertEquals(47.003057, $stations[0]->coordinate->y);
+        $this->assertEquals(47.003057, $stations[0]->coordinate->x);
+        $this->assertEquals(8.382324, $stations[0]->coordinate->y);
         $this->assertEquals('WGS84', $stations[0]->coordinate->type);
         $this->assertEquals('Bürgenstock, Hotels', $stations[1]->name);
-        $this->assertEquals(8.386252, $stations[1]->coordinate->x);
-        $this->assertEquals(46.997088, $stations[1]->coordinate->y);
+        $this->assertEquals(46.997088, $stations[1]->coordinate->x);
+        $this->assertEquals(8.386252, $stations[1]->coordinate->y);
         $this->assertEquals('WGS84', $stations[1]->coordinate->type);
     }
 

test/Transport/Test/Entity/LocationFactoryTest.php
@@ -51,8 +51,8 @@ EOF;
         $station->id = 8508489;
         $coordinate = new Coordinate();
         $coordinate->type = 'WGS84';
-        $coordinate->x = 8.382324;
-        $coordinate->y = 47.003057;
+        $coordinate->x = 47.003057;
+        $coordinate->y = 8.382324;
         $station->coordinate = $coordinate;
         $this->assertEquals($station, LocationFactory::createFromJson(json_decode($jsonString)));
     }
@@ -75,8 +75,8 @@ EOF;
         $station->id = 8508489;
         $coordinate = new Coordinate();
         $coordinate->type = 'WGS84';
-        $coordinate->x = 8.382324;
-        $coordinate->y = 47.003057;
+        $coordinate->x = 47.003057;
+        $coordinate->y = 8.382324;
         $station->coordinate = $coordinate;
         $this->assertEquals($station, LocationFactory::createFromJson(json_decode($jsonString)));
     }

commit db0e761929ff2a4a1c952ca8add5354c2731d377

Fixed distance calculation
Fixes #90, probably broken in 8e981ec8e9586d5779143627ade0f155a22c8069

lib/Transport/Entity/Coordinate.php
@@ -72,8 +72,8 @@ class Coordinate
     {
         $earth_radius = 6371;
 
-        $dLat = deg2rad($this->y - $lat);
-        $dLon = deg2rad($this->x - $lon);  
+        $dLat = deg2rad($this->x - $lat);
+        $dLon = deg2rad($this->y - $lon);  
 
         $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($lat)) * cos(deg2rad($this->y)) * sin($dLon/2) * sin($dLon/2);  
         $c = 2 * asin(sqrt($a));