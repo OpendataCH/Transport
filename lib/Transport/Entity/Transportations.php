7bb1b36780e3562db31ca86358d1b79e607f1b81
Fabian Vogler <fabian@equivalence.ch>
2015-11-30 08:35:31 +0100

    New implementation for Transportation logic
    SBB uses LSB 0 bit numbering for some requests

--- lib/Transport/Entity/Transportations.php
+++ lib/Transport/Entity/Transportations.php
@@ -5,42 +5,61 @@ namespace Transport\Entity;
 class Transportations
 {
     private static $transportationBits = array(
-        'all' => 65535, // 1<<16 - 1
-        'ice_tgv_rj' => 32768, // 1<<15
-        'ec_ic' => 16384, // 1<<14
-        'ir' => 8192, // 1<<13
-        're_d' => 4096, // 1<<12
-        'ship' => 2048, // 1<<11
-        's_sn_r' => 1024, // 1<<10
-        'bus' => 512, // 1<<9
-        'cableway' => 256, // 1<<8
-        'arz_ext' => 128, // 1<<7
-        'tramway_underground' => 64, // 1<<6
-        'tramway' => 32, // 1<<5
-        'direct' => 16, // 1<<4
-        'direct_sleeper' => 8, // 1<<3
-        'direct_couchette' => 4, // 1<<2
-        'bike' => 2, // 1<<1
-        'groups' => 1, // 1<<0
+        'ice_tgv_rj' => 1, // 2^0
+        'ec_ic' => 2, // 2^1
+        'ir' => 4, // 2^2
+        're_d' => 8, // 2^3
+        'ship' => 16, // 2^4
+        's_sn_r' => 32, // 2^5
+        'bus' => 64, // 2^6
+        'cableway' => 128, // 2^7
+        'arz_ext' => 256, // 2^8
+        'tramway_underground' => 512, // 2^9
+        'tramway' => 1024, // 2^10
+        'direct' => 2048, // 2^11
+        'direct_sleeper' => 4096, // 2^12
+        'direct_couchette' => 8192, // 2^13
+        'bike' => 16384, // 2^14
+        'groups' => 32768, // 2^15
     );
 
     /**
      * Converts a list of transportation strings into a bitmask accepted by the SBB
      *
      * @param   array[]string   $transportations    A list of transportations
+     * @param   int             $limit              Maximum number of transportations (bits)
+     * @param   boolean         $lsb0               Least significant bit first, LSB 0
      * @return  string          A binary representation of the transportations given
      */
-    public static function reduceTransportations($transportations = array())
+    public static function reduceTransportations($transportations = array(), $limit = 16, $lsb0 = true)
     {
-        $map = self::$transportationBits;
-        return sprintf('%016b', array_reduce($transportations, function ($aggr, $val) use ($map) { return $aggr + $map[$val]; }, 0));
+        $dec = self::reduceTransportationsDec($transportations, $limit);
+
+        $binary = sprintf('%0' . $limit . 'b', $dec);
+
+        if ($lsb0) {
+            // flip for SBB
+            $binary = strrev($binary);
+        }
+
+        return $binary;
     }
 
-    /**
-     * @return array
-     */
-    public function getTransportations()
+    public static function reduceTransportationsDec($transportations = array(), $limit = 16)
     {
-        return self::$transportations;
+        $dec = 0;
+
+        if (in_array('all', $transportations)) {
+
+            $dec = pow(2, $limit) - 1;
+
+        } else {
+
+            $map = self::$transportationBits;
+            $dec = array_reduce($transportations, function ($aggr, $val) use ($map) { return $aggr + $map[$val]; }, 0);
+
+        }
+
+        return $dec;
     }
 }
--- test/Transport/Test/Entity/TransportationsTest.php
+++ test/Transport/Test/Entity/TransportationsTest.php
@@ -12,4 +12,46 @@ class APITest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame('0000101000000000', Transportations::reduceTransportations($transportations));
     }
+
+    public function testReduceTransportationsTrain()
+    {
+        $transportations = array('ice_tgv_rj');
+
+        $this->assertSame('1000000000000000', Transportations::reduceTransportations($transportations));
+    }
+
+    public function testReduceTransportationsAll()
+    {
+        $transportations = array('all');
+
+        $this->assertSame('1111111111111111', Transportations::reduceTransportations($transportations));
+    }
+
+    public function testReduceTransportationsDec()
+    {
+        $transportations = array('bus', 'ship');
+
+        $this->assertSame(80, Transportations::reduceTransportationsDec($transportations));
+    }
+
+    public function testReduceTransportationsDecTrain()
+    {
+        $transportations = array('ice_tgv_rj');
+
+        $this->assertSame(1, Transportations::reduceTransportationsDec($transportations));
+    }
+
+    public function testReduceTransportationsDecAll()
+    {
+        $transportations = array('all');
+
+        $this->assertSame(65535, Transportations::reduceTransportationsDec($transportations));
+    }
+
+    public function testReduceTransportationsDecAllTen()
+    {
+        $transportations = array('all');
+
+        $this->assertSame(1023, Transportations::reduceTransportationsDec($transportations, 10));
+    }
 }
