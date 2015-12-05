4bd0de1b70c82aaa7609d4eaf4135404a2e417d2
Fabian Vogler <fabian@equivalence.ch>
2015-12-05 12:44:07 +0100

    Remove Boxfile

--- pagodabox.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$app['debug'] = false;
-$app['http_cache'] = true;
-$app['buzz.client'] = new Buzz\Client\Curl();
-$app['xhprof'] = false;
-$app['monolog.level'] = Monolog\Logger::INFO;
-$app['redis.config'] = array('host' => $_SERVER['CACHE1_HOST'], 'port' => $_SERVER['CACHE1_PORT']);
-$app['proxy'] = true;
