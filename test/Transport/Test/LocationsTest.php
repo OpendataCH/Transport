a7219c989eb2d8b50e65ed67b173a356397e9279
Fabian Vogler <fabian@equivalence.ch>
2015-12-23 14:33:23 +0100

    Move all location tests to LocationsTest

--- test/Transport/Test/LocationsTest.php
+++ test/Transport/Test/LocationsTest.php
@@ -6,6 +6,14 @@ use Buzz\Message\Response;
 
 class LocationsTest extends IntegrationTest
 {
+    private $url = 'http://fahrplan.sbb.ch/bin/extxml.exe/';
+
+    private $headers = array(
+        'User-Agent: SBBMobile/4.8 CFNetwork/609.1.4 Darwin/13.0.0',
+        'Accept: application/xml',
+        'Content-Type: application/xml'
+    );
+
     public function testGetLocation()
     {
         $response = new Response();
@@ -14,7 +22,7 @@ class LocationsTest extends IntegrationTest
         $this->getBrowser()->expects($this->any())
             ->method('post')
             ->withConsecutive(
-                array($this->anything(), $this->anything(), $this->equalTo($this->getXmlFixture('locations/hafas_request.xml')))
+                array($this->equalTo($this->url), $this->equalTo($this->headers), $this->equalTo($this->getXmlFixture('locations/hafas_request.xml')))
             )
             ->will($this->onConsecutiveCalls($response));
 
@@ -27,25 +35,33 @@ class LocationsTest extends IntegrationTest
         $this->assertTrue($client->getResponse()->isOk());
     }
 
-    public function testGetNearbyLocation()
+    public function nearbyProvider()
+    {
+        return array(
+            array(array('x' => '47.002347', 'y' => '8.379934'), 'http://fahrplan.sbb.ch/bin/query.exe/dny?performLocating=2&tpl=stop2json&look_maxno=10&look_stopclass=1023&look_maxdist=5000&look_y=47002347&look_x=8379934', 'hafas_response_nearby.json', 'response_nearby.json'),
+            array(array('x' => '46.388653', 'y' => '6.238729'), 'http://fahrplan.sbb.ch/bin/query.exe/dny?performLocating=2&tpl=stop2json&look_maxno=10&look_stopclass=1023&look_maxdist=5000&look_y=46388653&look_x=6238729', 'hafas_response_nyon.json', 'response_nyon.json'),
+        );
+    }
+
+    /**
+     * @dataProvider nearbyProvider
+     */
+    public function testGetNearbyLocation($parameters, $requestUrl, $hafasResponse, $jsonReponse)
     {
         $response = new Response();
-        $response->setContent($this->getFixture('locations/hafas_response_nearby.json'));
+        $response->setContent($this->getFixture('locations/' . $hafasResponse));
 
         $this->getBrowser()->expects($this->any())
             ->method('get')
             ->withConsecutive(
-                array('http://fahrplan.sbb.ch/bin/query.exe/dny?performLocating=2&tpl=stop2json&look_maxno=10&look_stopclass=1023&look_maxdist=5000&look_y=47002347&look_x=8379934')
+                array($requestUrl)
             )
             ->will($this->onConsecutiveCalls($response));
 
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/v1/locations', array(
-            'x' => '47.002347',
-            'y' => '8.379934',
-        ));
+        $crawler = $client->request('GET', '/v1/locations', $parameters);
 
-        $this->assertEquals($this->getFixture('locations/response_nearby.json'), $this->json($client->getResponse()));
+        $this->assertEquals($this->getFixture('locations/' . $jsonReponse), $this->json($client->getResponse()));
         $this->assertTrue($client->getResponse()->isOk());
     }
 }
