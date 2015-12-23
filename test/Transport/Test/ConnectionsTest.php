8a529b2420d2773f2c7b96cc4aa4c5cbb4531b9e
Fabian Vogler <fabian@equivalence.ch>
2015-12-23 14:24:24 +0100

    Add another connections fixture

--- test/Transport/Test/ConnectionsTest.php
+++ test/Transport/Test/ConnectionsTest.php
@@ -6,31 +6,45 @@ use Buzz\Message\Response;
 
 class ConnectionsTest extends IntegrationTest
 {
-    public function testGetConnections()
+    private $url = 'http://fahrplan.sbb.ch/bin/extxml.exe/';
+
+    private $headers = array(
+        'User-Agent: SBBMobile/4.8 CFNetwork/609.1.4 Darwin/13.0.0',
+        'Accept: application/xml',
+        'Content-Type: application/xml'
+    );
+
+    public function connectionsProvider()
+    {
+        return array(
+            array(array('from' => 'Zürich', 'to' => 'Bern', 'date' => '2012-01-31', 'time' => '23:55:00'), 'hafas_request_2012-01-31.xml', 'hafas_response_2012-01-31.xml', 'response_2012-01-31.json'),
+            array(array('from' => 'Zürich', 'to' => 'Bern', 'date' => '2012-12-23', 'time' => '14:30:00'), 'hafas_request_2015-12-23.xml', 'hafas_response_2015-12-23.xml', 'response_2015-12-23.json'),
+        );
+    }
+
+    /**
+     * @dataProvider connectionsProvider
+     */
+    public function testGetConnections($parameters, $hafasRequest, $hafasResponse, $response)
     {
         $responseLocation = new Response();
         $responseLocation->setContent($this->getFixture('connections/hafas_response_location.xml'));
 
         $responseConnection = new Response();
-        $responseConnection->setContent($this->getFixture('connections/hafas_response_2012-01-31.xml'));
+        $responseConnection->setContent($this->getFixture('connections/' . $hafasResponse));
 
         $this->getBrowser()->expects($this->any())
             ->method('post')
             ->withConsecutive(
-                array($this->anything(), $this->anything(), $this->equalTo($this->getXmlFixture('connections/hafas_request_location.xml'))),
-                array($this->anything(), $this->anything(), $this->equalTo($this->getXmlFixture('connections/hafas_request_2012-01-31.xml')))
+                array($this->equalTo($this->url), $this->equalTo($this->headers), $this->equalTo($this->getXmlFixture('connections/hafas_request_location.xml'))),
+                array($this->equalTo($this->url), $this->equalTo($this->headers), $this->equalTo($this->getXmlFixture('connections/' . $hafasRequest)))
             )
             ->will($this->onConsecutiveCalls($responseLocation, $responseConnection));
 
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/v1/connections', array(
-            'from' => 'Zürich',
-            'to' => 'Bern',
-            'date' => '2012-01-31',
-            'time' => '23:55:00',
-        ));
+        $crawler = $client->request('GET', '/v1/connections', $parameters);
 
-        $this->assertEquals($this->getFixture('connections/response_2012-01-31.json'), $this->json($client->getResponse()));
+        $this->assertEquals($this->getFixture('connections/' . $response), $this->json($client->getResponse()));
         $this->assertTrue($client->getResponse()->isOk());
     }
 }
