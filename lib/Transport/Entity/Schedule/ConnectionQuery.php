9e2a49dfe411d05c3c05f46dd5be3d6edc413cbe
Fabian Vogler <fabian@equivalence.ch>
2017-06-21 07:16:56 +0200

    Add support for isArrivalTime

--- lib/Transport/Entity/Schedule/ConnectionQuery.php
+++ lib/Transport/Entity/Schedule/ConnectionQuery.php
@@ -67,6 +67,7 @@ class ConnectionQuery extends Query
         $request->setField('via', $this->viaLocations);
         $request->setField('date', date('Y-m-d', strtotime($this->date)));
         $request->setField('time', date('H:i', strtotime($this->time)));
+        $request->setField('time_type', $this->isArrivalTime ? 'arrival' : 'departure');
         $request->setField('show_delays', '1');
 
         return $request;
